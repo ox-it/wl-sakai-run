cea71bd1e34d08de6e29555d6bee2e57c1ddf690
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-02-01 13:07:58 +0000

    Kill the tail

--- postbuild.sh
+++ postbuild.sh
@@ -8,13 +8,12 @@ if [ -d $CATALINA_HOME ] ; then
   BUILD_ID=dontKillMe $CATALINA_HOME/bin/catalina.sh start
   mkfifo $WORKSPACE/target/log-fifo
   tail -f $WORKSPACE/target/tomcat/logs/catalina.out | tee  $WORKSPACE/target/log-fifo &
-  tail_pid=$!
+  # So we kill tail.
+  tail_pid=`jobs -p %1`
   if grep -q "Server startup in " < $WORKSPACE/target/log-fifo ; then
     echo yey
   fi
   echo $tail_pid
   kill $tail_pid
-  # Make sure tail dies 
-  echo die die die tail..... >> $WORKSPACE/target/tomcat/logs/catalina.out
   wait $tail_pid
 fi
