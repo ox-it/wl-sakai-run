5ef7c7252ef2088bc407a3edda64d4e96259fd4c
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-02-01 12:38:22 +0000

    Fix the subshell

--- postbuild.sh
+++ postbuild.sh
@@ -7,7 +7,7 @@ if [ -d $CATALINA_HOME ] ; then
   # http://issues.hudson-ci.org/browse/HUDSON-2729
   BUILD_ID=dontKillMe $CATALINA_HOME/bin/catalina.sh start
   mkfifo $WORKSPACE/target/log-fifo
-  (tail -f $WORKSPACE/target/tomcat/logs/catalina.out | tee -) > $WORKSPACE/target/log-fifo &
+  (tail -f $WORKSPACE/target/tomcat/logs/catalina.out | tee  $WORKSPACE/target/log-fifo )&
   if grep -q "Server startup in " < $WORKSPACE/target/log-fifo ; then
     echo yey
   fi
