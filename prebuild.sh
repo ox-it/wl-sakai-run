2d63e5426c6c31346ce70d0b373c650399bcaa11
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-02-01 12:40:29 +0000

    Don't fail build when can't stop tomcat

--- prebuild.sh
+++ prebuild.sh
@@ -4,5 +4,7 @@
 . $WORKSPACE/common.sh
 if [ -d $CATALINA_HOME ] ; then
   chmod +x $CATALINA_HOME/bin/*.sh
-  $CATALINA_HOME/bin/catalina.sh stop -force
+  if $CATALINA_HOME/bin/catalina.sh stop -force ; then
+    echo Tomcat stopped
+  fi
 fi
