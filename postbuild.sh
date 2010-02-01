95725fa8251b59cdc450b558c79c646660051a70
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-02-01 16:05:54 +0000

    Remove unneeded references to workspace.

--- postbuild.sh
+++ postbuild.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Script executed after the build
 
-. $WORKSPACE/common.sh
+. common.sh
 if [ -d $CATALINA_HOME ] ; then
   chmod +x $CATALINA_HOME/bin/*.sh
   # http://issues.hudson-ci.org/browse/HUDSON-2729
   BUILD_ID=dontKillMe $CATALINA_HOME/bin/catalina.sh start
-  mkfifo $WORKSPACE/target/log-fifo
-  tail -f $WORKSPACE/target/tomcat/logs/catalina.out | tee  $WORKSPACE/target/log-fifo &
-  if grep -q "Server startup in " < $WORKSPACE/target/log-fifo ; then
-    echo yey
+  mkfifo target/log-fifo
+  tail -f target/tomcat/logs/catalina.out | tee  target/log-fifo &
+  if grep -q "Server startup in " < target/log-fifo ; then
+    echo Found startup message
   fi
   # If this happens too quickly then the background job won't be up and running yet.
   tail_pid=`jobs -p %1`
--- prebuild.sh
+++ prebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Script executed before the build
 
-. $WORKSPACE/common.sh
+. common.sh
 if [ -d $CATALINA_HOME -a -f $CATALINA_PID ] ; then
   chmod +x $CATALINA_HOME/bin/*.sh
   if $CATALINA_HOME/bin/catalina.sh stop -force ; then
