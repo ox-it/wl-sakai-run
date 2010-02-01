038fc24ae05755b6a03af7f7110f44045209a613
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-02-01 10:51:25 +0000

    Fixed source of file

--- prebuild.sh
+++ prebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Script executed before the build
 
-source $WORKSPACE/common.sh
+. $WORKSPACE/common.sh
 if [ -d $CATALINA_HOME ] ; then
   $CATALINA_HOME/bin/catalina.sh stop --force
 fi
