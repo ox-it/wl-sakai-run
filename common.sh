0b3862f0860770c8b03de2f3ec1a42ab714547ec
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-02-01 16:03:35 +0000

    Allow scripts to be run outside hudson.

--- common.sh
+++ common.sh
@@ -1,5 +1,6 @@
 # Setup common things for both the pre and post scripts.
 
+[ "$WORKSPACE" ] || WORKSPACE=`pwd`
 export CATALINA_PID="$WORKSPACE/target/catalina-buckett.pid"
 export CATALINA_OPTS="-server -Xms256m -Xmx1024m -XX:NewSize=192m -XX:MaxNewSize=384m -XX:PermSize=192m -XX:MaxPermSize=256m -Djava.awt.headless=true -Dsun.net.inetaddr.ttl=0 -Dsun.lang.ClassLoader.allowArraySyntax=true"
 export CATALINA_HOME="$WORKSPACE/target/tomcat/"
