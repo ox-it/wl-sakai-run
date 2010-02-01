70edba22d32ae6992e8ee7d7a7a0649dfba9901c
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-02-01 16:05:20 +0000

    Cleanout MySQL db.

--- prebuild.sh
+++ prebuild.sh
@@ -7,4 +7,13 @@ if [ -d $CATALINA_HOME -a -f $CATALINA_PID ] ; then
   if $CATALINA_HOME/bin/catalina.sh stop -force ; then
     echo Tomcat stopped
   fi
+  
+  # Current user needs access to the database
+  # -s No boxing -N No column names
+  db=test
+  (mysql $db -e 'show tables' -sN | while read table
+  do
+    echo DROP TABLE $table;
+  done) | mysql $db
+ 
 fi
