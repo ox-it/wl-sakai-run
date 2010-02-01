bd8fb7f2b93816ad25b9b705c3a5c8565a6a28ed
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-02-01 11:59:16 +0000

    Fix the force option

--- prebuild.sh
+++ prebuild.sh
@@ -4,5 +4,5 @@
 . $WORKSPACE/common.sh
 if [ -d $CATALINA_HOME ] ; then
   chmod +x $CATALINA_HOME/bin/*.sh
-  $CATALINA_HOME/bin/catalina.sh stop --force
+  $CATALINA_HOME/bin/catalina.sh stop -force
 fi
