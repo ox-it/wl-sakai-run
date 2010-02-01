dbb00dfef27a64f6d02084ff1cca0cc008088897
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-02-01 16:05:08 +0000

    Make sure we have a PID file to kill with.

--- prebuild.sh
+++ prebuild.sh
@@ -2,7 +2,7 @@
 # Script executed before the build
 
 . $WORKSPACE/common.sh
-if [ -d $CATALINA_HOME ] ; then
+if [ -d $CATALINA_HOME -a -f $CATALINA_PID ] ; then
   chmod +x $CATALINA_HOME/bin/*.sh
   if $CATALINA_HOME/bin/catalina.sh stop -force ; then
     echo Tomcat stopped
