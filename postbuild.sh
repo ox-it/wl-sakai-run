9298f4445f735406fb6d431f201865eac16c2cd6
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-02-01 12:47:54 +0000

    Return to capturing the PID.

--- postbuild.sh
+++ postbuild.sh
@@ -7,10 +7,12 @@ if [ -d $CATALINA_HOME ] ; then
   # http://issues.hudson-ci.org/browse/HUDSON-2729
   BUILD_ID=dontKillMe $CATALINA_HOME/bin/catalina.sh start
   mkfifo $WORKSPACE/target/log-fifo
-  (tail -f $WORKSPACE/target/tomcat/logs/catalina.out | tee  $WORKSPACE/target/log-fifo )&
+  tail -f $WORKSPACE/target/tomcat/logs/catalina.out | tee  $WORKSPACE/target/log-fifo &
+  tail_pid=$!
   if grep -q "Server startup in " < $WORKSPACE/target/log-fifo ; then
     echo yey
   fi
-  kill %1
-  wait %1
+  echo $tail_pid
+  kill $tail_pid
+  wait $tail_pid
 fi
