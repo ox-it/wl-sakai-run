2b81cfe64ba3edd650a05d949791443d29eaeb2f
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2012-01-27 15:43:57 +0000

    Increased permgen space as 2.8 needs more

--- common.sh
+++ common.sh
@@ -2,5 +2,5 @@
 
 [ "$WORKSPACE" ] || WORKSPACE=`pwd`
 export CATALINA_PID="$WORKSPACE/target/catalina-buckett.pid"
-export CATALINA_OPTS="-server -Xms256m -Xmx1024m -XX:NewSize=192m -XX:MaxNewSize=384m -XX:PermSize=192m -XX:MaxPermSize=256m -Djava.awt.headless=true -Dsun.net.inetaddr.ttl=0 -Dsun.lang.ClassLoader.allowArraySyntax=true"
+export CATALINA_OPTS="-server -Xms256m -Xmx1024m -XX:NewSize=192m -XX:MaxNewSize=384m -XX:PermSize=192m -XX:MaxPermSize=384m -Djava.awt.headless=true -Dsun.net.inetaddr.ttl=0 -Dsun.lang.ClassLoader.allowArraySyntax=true"
 export CATALINA_HOME="$WORKSPACE/target/tomcat/"
