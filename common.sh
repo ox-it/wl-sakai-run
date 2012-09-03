ff28461e8314500654a64fc7bedc08c9f624f567
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-09-03 11:31:37 +0100

    Set solr.solr.home before starting tomcat

--- common.sh
+++ common.sh
@@ -2,6 +2,6 @@
 
 [ "$WORKSPACE" ] || WORKSPACE=`pwd`
 export CATALINA_PID="$WORKSPACE/target/catalina-buckett.pid"
-export CATALINA_OPTS="-server -Xms256m -Xmx1024m -XX:NewSize=192m -XX:MaxNewSize=384m -XX:PermSize=192m -XX:MaxPermSize=384m -Djava.awt.headless=true -Dsun.net.inetaddr.ttl=0 -Dsun.lang.ClassLoader.allowArraySyntax=true"
+export CATALINA_OPTS="-server -Xms256m -Xmx1024m -XX:NewSize=192m -XX:MaxNewSize=384m -XX:PermSize=192m -XX:MaxPermSize=384m -Djava.awt.headless=true -Dsun.net.inetaddr.ttl=0 -Dsun.lang.ClassLoader.allowArraySyntax=true -Dsolr.solr.home=multicore"
 export CATALINA_HOME="$WORKSPACE/target/tomcat/"
 export JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=$CATALINA_HOME/solr/"
