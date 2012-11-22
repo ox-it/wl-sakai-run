4daede76672dadc058560f5c9e4f8cdb23965c34
Colin Hebert <colin.hebert@it.ox.ac.uk>
2012-11-22 11:48:57 +0000

    Move log4j.properties to common/classes/

--- postbuild.sh
+++ postbuild.sh
@@ -7,15 +7,15 @@ if [ -d $CATALINA_HOME ] ; then
 
   # Grab config from svn
   svn export https://svn.oucs.ox.ac.uk/projects/vle/deployment/debian/sakai/config/trunk/sakai.properties target/tomcat/sakai/sakai.properties
-  svn export https://svn.oucs.ox.ac.uk/projects/vle/sakai/config/trunk/log4j.properties target/tomcat/sakai/log4j.properties
   svn export https://svn.oucs.ox.ac.uk/projects/vle/sakai/config/trunk/local.properties target/tomcat/sakai/local.properties
+  svn export https://svn.oucs.ox.ac.uk/projects/vle/sakai/config/trunk/log4j.properties target/tomcat/common/classes/log4j.properties
   svn export --force https://svn.oucs.ox.ac.uk/projects/vle/deployment/solr-config target/tomcat/solr
   sed -i "s/DB_NAME/${DB_NAME}/" target/tomcat/sakai/local.properties
   sed -i "s|SEARCH_SOLR_URL|${SEARCH_SOLR_URL}|" target/tomcat/sakai/local.properties
   sed -i "s/TURNITIN_AID/${TURNITIN_AID:-69293}/" target/tomcat/sakai/local.properties
   sed -i "s/TURNITIN_SECRET/${TURNITIN_SECRET:-OUCSsKey}/" target/tomcat/sakai/local.properties
   sed -i "s|TURNITIN_API_URL|${TURNITIN_API_URL:-'https://sandbox.turnitin.com/api.asp'}|" target/tomcat/sakai/local.properties
-  sed -i "s|SENTRY_DSN|${SENTRY_DSN}|" target/tomcat/sakai/log4j.properties
+  sed -i "s|SENTRY_DSN|${SENTRY_DSN}|" target/tomcat/common/classes/log4j.properties
 
   # Show the build version that we're running.
   echo version.service=$BUILD_TAG >> target/tomcat/sakai/local.properties
