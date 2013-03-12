52616f8a59492de4842a501b6de9179006b5a704
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2013-03-12 14:10:37 +0000

    Don't lose the PID file if the workspace is lost.

--- common.sh
+++ common.sh
@@ -1,7 +1,9 @@
 # Setup common things for both the pre and post scripts.
 
 [ "$WORKSPACE" ] || WORKSPACE=`pwd`
-export CATALINA_PID="$WORKSPACE/target/catalina-buckett.pid"
+# We keep this outside the workspace so that if the workspace gets wiped out 
+# we can still terminate the process
+export CATALINA_PID="/tmp/${JOB_NAME}-catalina.pid"
 export CATALINA_OPTS="-server -Xms256m -Xmx1024m -XX:NewSize=192m -XX:MaxNewSize=384m -XX:PermSize=192m -XX:MaxPermSize=384m -Djava.awt.headless=true -Dsun.net.inetaddr.ttl=0 -Dsun.lang.ClassLoader.allowArraySyntax=true"
 export CATALINA_HOME="$WORKSPACE/target/tomcat/"
 export JAVA_OPTS="$JAVA_OPTS -Dsolr.solr.home=$CATALINA_HOME/solr/"
